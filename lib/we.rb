b0d241e280e5455d6ce8809bc9f4e3412b4fbe65
richardemslie <richard@clue.co.za>
2012-10-20 18:13:00 +0200

    call into nested blocks

--- lib/we.rb
+++ lib/we.rb
@@ -2,12 +2,18 @@ module We; end
 
 def we( *args, &block )
 
+  block.call if block
+
 end
 
 def as( *args, &block )
 
+  block.call if block
+
 end
 
 def to( *args, &block )
+
+  block.call if block
 
 end
