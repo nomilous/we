2b9e2f8ec5195145d8d3f682a3dc9517527a5a38
richardemslie <richard@clue.co.za>
2012-10-25 01:39:40 +0200

    node stores data

--- lib/we/node/node.rb
+++ lib/we/node/node.rb
@@ -2,6 +2,22 @@ module We
 
   class Node
 
+    attr_reader :data
+
+    def initialize
+
+      @data = {}
+
+    end
+
+    def store( data )
+
+      @data.merge!( data ) do |key, old, new|
+
+
+      end
+
+    end
 
   end
 
--- spec/we/node/node_spec.rb
+++ spec/we/node/node_spec.rb
@@ -1,4 +1,19 @@
 describe We::Node do 
 
+  subject { We::Node.new }
+
+  it 'has data' do
+
+    subject.data.should be_a Hash
+
+  end
+
+  it 'can accumulate data' do
+
+    subject.store new: 'data'
+
+    subject.data.should == { new: 'data' }
+
+  end
 
 end
