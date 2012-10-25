e79a1901da8d1b7628a9ca96bf7c8fbf9969a629
richardemslie <richard@clue.co.za>
2012-10-25 23:29:01 +0200

    undefined method 'define'

---
+++ lib/we/define/define.rb
@@ -0,0 +1,11 @@
+module We
+
+  class << self
+
+    def define
+
+    end
+
+  end
+
+end
---
+++ spec/we/define/define_spec.rb
@@ -0,0 +1,13 @@
+describe 'We::define' do
+
+  it 'defines' do
+
+    We::define 
+
+    #
+    # undefined method `define' for We:Module
+    # 
+
+  end
+
+end
