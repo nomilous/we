656bfc475fa406fde2b43f419f790071a2f3f7f9
richardemslie <richard@clue.co.za>
2012-10-27 10:23:23 +0200

    humour |noun| ~ we love it

--- spec/we/validate/validate_spec.rb
+++ spec/we/validate/validate_spec.rb
@@ -2,14 +2,14 @@ describe 'We::validate' do
 
   it 'warns on undefined' do
 
-    We::should_receive( :warn )
+    We::should_receive( :captains_log ).with( stardate, message )
 
     we activate: 'uncertainty shields'
     we activate: 'duality field'
     we activate: 'supersymmetry sensor array'
     we activate: 'entanglement phase array'
     we activate: 'causality phase array'
-
+    
     we rotate: 'chaos manifold', in: [3,2,1,]
 
   end
