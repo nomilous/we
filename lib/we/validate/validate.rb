7d1401b15cf9db110c75240c82cd1b9d5a0017b4
richardemslie <richard@clue.co.za>
2012-10-27 12:09:43 +0200

    validate

--- lib/we/validate/validate.rb
+++ lib/we/validate/validate.rb
@@ -4,6 +4,47 @@ module We
 
     def validate( args )
 
+      unless We::defined[We::primary args]
+      
+        We::log We::warning UNKNOWN_ACTION, args
+
+        return We::Node
+
+      end
+
+      return We::defined[We::primary args]
+
+    end
+
+    def primary( args )
+
+      if args.is_a? Hash
+
+        args.keys.first
+
+      elsif args.is_a? Array
+
+        args.first
+
+      elsif args.is_a? Symbol
+
+        args
+
+      else 
+
+        #
+        # to be decided:
+        # 
+        #  we 'string' do
+        #  we 3 do 
+        # 
+        # some ideas pop to mind
+        # 
+
+        args
+
+      end
+
     end
 
   end
--- spec/we/validate/validate_spec.rb
+++ spec/we/validate/validate_spec.rb
@@ -1,16 +1,45 @@
 describe 'We::validate' do
 
+  it 'extracts primary from args when Hash' do
+
+    We::primary( document: 'Title' ).should == :document
+
+  end
+
+  it 'extracts primary from args when Array' do
+
+    We::primary( [:fragment, :mode_] ).should == :fragment
+
+  end
+
+  it 'extracts primary from args single symbol' do
+
+    We::primary( :fragment ).should == :fragment
+
+  end
+
   it 'warns on undefined' do
 
-    We::should_receive( :captains_log ).with( stardate, message )
+    pending 'We::warning'
+
+    We::should_receive( :warn )
 
     we activate: 'uncertainty shields'
     we activate: 'duality field'
     we activate: 'supersymmetry sensor array'
     we activate: 'entanglement phase array'
     we activate: 'causality phase array'
-    
-    we rotate: 'chaos manifold', in: [3,2,1,]
+
+    we rotate: 'chaos manifold', in: [3,2,1,] do
+
+    end
+
+  end
+
+  it 'returns the Node type' do
+
+    type = We::validate document: 'Title'
+    type.should == We::Document
 
   end
 
