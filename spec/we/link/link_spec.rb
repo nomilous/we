f9a44b04a5986774669389b6bec4a2ee5cc91f7c
richardemslie <richard@clue.co.za>
2012-10-28 13:09:06 +0200

    todo

--- spec/we/link/link_spec.rb
+++ spec/we/link/link_spec.rb
@@ -1,5 +1,24 @@
 describe 'We::link' do
 
+
+      #
+      # TODO: Scope awareness on the link: directive
+      #        
+      #       - plugins will say: 
+      #        
+      #         we link: 'a parameter'
+      # 
+      #       - that will have different meanings for
+      #         different plugins
+      # 
+      #       - walker needs to detect the edges of
+      #         plugin scope 
+      # 
+      #         keeping a plugin scope stack
+      # 
+      #         each a their own link Node responder
+      # 
+
   it 'links' do
 
     We::link
